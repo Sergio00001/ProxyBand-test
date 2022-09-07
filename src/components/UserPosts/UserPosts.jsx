9b34accd29cb56458ceea12cfc338787875fb017
Sergio00001 <digtyarsergio@gmail.com>
2022-09-07 17:15:33 +0300

    add adaptive layout

--- src/components/UserPosts/UserPosts.jsx
+++ src/components/UserPosts/UserPosts.jsx
@@ -4,7 +4,7 @@ import './UserPosts.css'
 export const UserPosts = ({ post }) => {
     return (
         <div className='user__post border-2 border-green-600 rounded my-2 py-4 px-6 bg-transparent'>
-            <h1 className='text-center font-bold'>{post.title}</h1>
+            <h1 className='text-center font-bold mb-4'>{post.title}</h1>
             <p className='text-center'>{post.body}</p>
         </div>
     )
